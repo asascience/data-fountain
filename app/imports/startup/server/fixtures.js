37968fd0e88b7c80f9c83eab292b1f12a7e3701b
FBRTMaka <mjmcee@gmail.com>
2016-05-11 13:54:20 -0400

    prep for deployment

--- app/imports/startup/server/fixtures.js
+++ app/imports/startup/server/fixtures.js
@@ -21,7 +21,7 @@ Meteor.startup(() => {
 
         // go and get the stations, and convert the heathen snake case to
         // glorious camel case.
-        let response = HTTP.get(process.env.DATA_FOUNTAIN_URL),
+        let response = HTTP.get(Meteor.settings.dataFountainUrl),
             snakeData = EJSON.fromJSONValue(response.data.stations),
             data = HUMPS.camelizeKeys(snakeData);
 
@@ -30,12 +30,17 @@ Meteor.startup(() => {
             Stations.insert(station);
         });
     } catch (exception) {
-        let error = {
-            code: exception.response.statusCode,
-            url: process.env.DATA_FOUNTAIN_URL,
-            data: exception.response.data
-        };
+        if (exception.response) {
+            let error = {
+                code: exception.response.statusCode,
+                url: process.env.DATA_FOUNTAIN_URL,
+                data: exception.response.data
+            };
 
-        throw new Meteor.Error('500', `\r\n\tThe server has thrown a 500 error, this is because there was a problem connecting to OceansMap.\r\n\tOceansMap connection responded with:\n\r\t\t ${EJSON.stringify(error)}\n\r\tMake sure the URL is correct, and that data is flowing.\r\n\tIf the problem persists, you'll need to call for help.`);
+            throw new Meteor.Error('500', `\r\n\tThe server has thrown a 500 error, this is because there was a problem connecting to OceansMap.\r\n\tOceansMap connection responded with:\n\r\t\t ${EJSON.stringify(error)}\n\r\tMake sure the URL is correct, and that data is flowing.\r\n\tIf the problem persists, you'll need to call for help.`);
+        } else {
+
+            throw new Meteor.Error('500', `Unhandled exception: ${exception}`);
+        }
     }
 });
