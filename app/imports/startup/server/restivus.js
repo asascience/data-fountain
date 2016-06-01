cc8366e5b1b4a969ee76cbb8116e6e80546e98c6
FBRTMaka <mjmcee@gmail.com>
2016-06-01 10:26:22 -0400

    api routes

--- app/imports/startup/server/restivus.js
+++ app/imports/startup/server/restivus.js
@@ -8,4 +8,29 @@ if (Meteor.isServer) {
 
     Api.addCollection(Stations);
     Api.addCollection(Data);
+
+
+    Api.addRoute('data/', {authRequired: false}, {
+        get() {
+            return Data.find({}).fetch();
+        }
+    });
+
+    Api.addRoute('data/:id', {authRequired: false}, {
+        get() {
+            return Data.findOne({id: this.urlParams.id});
+        }
+    });
+
+    Api.addRoute('stations', {authRequired: false}, {
+        get() {
+            return Stations.find({}).fetch();
+        }
+    });
+
+    Api.addRoute('stations/:id', {authRequired: false}, {
+        get() {
+            return Stations.findOne({id: this.urlParams.id});
+        }
+    });
 }
